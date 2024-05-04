76085ab32289f6f8796ac03362f68275de527a2b
bijanghanei <iambijanghanei@gmail.com>
2024-05-04 21:12:11 +0330

    config correction done

--- src/main/java/com/bijanghanei/bex/config/BotConfig.java
+++ src/main/java/com/bijanghanei/bex/config/BotConfig.java
@@ -1,7 +1,16 @@
 package com.bijanghanei.bex.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
+@PropertySource("application.properties")
 public class BotConfig {
+    @Value("${bot.name}")
+    private String botName;
+    @Value("${bot.token}")
+    private String botToken;
 }
